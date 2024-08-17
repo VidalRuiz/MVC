abd3862803be08d7f1f576821d39d163b0263375
Rafael <rafael.glez@gmail.com>
2024-08-16 21:05:06 -0600

    Initial version

---
+++ MVC/Model/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  MVC
+//
+//  Created by Rafael Gonzalez on 16/08/24.
+//
+
+import Foundation
---
+++ MVC/Model/PokemonDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonDataManager.swift
+//  MVC
+//
+//  Created by Rafael Gonzalez on 16/08/24.
+//
+
+import Foundation
